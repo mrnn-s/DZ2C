1d4bfc5ebee9971e413992e6ddd47fe9fe562a9a
MariannaSarkisian <spn.mjj@gmail.com>
2022-10-06 21:31:41 +0300

    4

---
+++ 4zad/Program.cs
@@ -0,0 +1,19 @@
+﻿// Программа проверяет пятизначное число на палиндромом.
+
+Console.WriteLine("Введите пятизначное число");
+int chislo = int.Parse(Console.ReadLine() ?? "0");
+int a = (chislo/10000);
+int b = ((chislo%10000)/1000);
+int c = (((chislo%10000)%1000)/100);
+int d = ((((chislo%10000)%1000)%100)/10);
+int e = ((((chislo/10000)%1000)%100)%10);
+int result=(e*10000+d*1000+c*100+b*10+a);
+
+if (result==chislo)
+{
+    Console.WriteLine ("POLINOM");
+}
+else
+{
+    Console.WriteLine ("NEPOLINOM");
+}
