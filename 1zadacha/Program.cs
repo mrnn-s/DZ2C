f346eae1b4abcf8151d546ca6f79e09e18ac8f3d
MariannaSarkisian <spn.mjj@gmail.com>
2022-10-06 20:43:07 +0300

    1 and 2 zadac

---
+++ 1zadacha/Program.cs
@@ -0,0 +1,40 @@
+﻿//Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+Console.WriteLine("enter number of the day of week from 1-7" );
+int Day = int.Parse(Console.ReadLine() ?? "0");
+if (Day ==1)
+{
+   Console.WriteLine("Monday,weekday");  
+}
+if (Day==2)
+{
+      Console.WriteLine("tuesday,weekday");
+}
+if (Day==3)
+{
+    Console.WriteLine("wednesday,weekdar");
+}
+if (Day==4)
+{
+  Console.WriteLine("thursday,weekday");
+}
+if (Day==5)
+{
+    Console.WriteLine("Friday,weekday");
+}
+if (Day==6)
+{
+    Console.WriteLine("SATURDAY.WEEKEND");
+}
+if (Day==7)
+{
+    Console.WriteLine("SUNDAY.WEEKEND");
+}
+if (Day>7)
+{
+    Console.WriteLine("wrong,try again (number from 1-to 7)");
+}
+if (Day<=0)
+{
+    Console.WriteLine("wrong,try again (number from 1-to 7)");
+}
---
+++ 2zadacha/Program.cs
@@ -0,0 +1,22 @@
+﻿//По двум заданным числам проверять является ли одно квадратом другого
+
+
+Console.WriteLine("enter number1" );
+int number1 = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("enter number2" );
+int number2 = int.Parse(Console.ReadLine() ?? "0");
+double kvadrat = Math.Pow(number1, 2);
+double kvadrat2 = Math.Pow(number2, 2);
+if (number1 == kvadrat2)
+{
+    Console.WriteLine("Первое число есть квадрат второго");
+}
+else if (number2 == kvadrat)
+{
+    Console.WriteLine("Второе число есть квадрат первого");
+}
+else
+{
+    Console.WriteLine("Одно число не является квадратом другого");
+}
