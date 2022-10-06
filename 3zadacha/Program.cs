80e37c7ae6be72782e95617fd8328f0c2889631b
MariannaSarkisian <spn.mjj@gmail.com>
2022-10-06 20:50:48 +0300

    po nov 3 zad s break i switch

---
+++ 3zadacha/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задать номер четверти, показать диапазоны для возможных координат
+Console.Write("enter nomer chetverti:");
+int chetvert = int.Parse(Console.ReadLine() ?? "0");
+if (chetvert <= 4)
+    switch (chetvert)
+    {
+        case 1:
+            Console.WriteLine("0<X<(+)Бесконечность, 0<Y<(+)Бесконечность");
+            break;
+        case 2:
+            Console.WriteLine("-Бесконечность<X<0, 0<Y<(+)Бесконечность");
+            break;
+        case 3:
+            Console.WriteLine("-Бесконечность<X<0, -Бесконечность<Y<0");
+            break;
+        case 4:
+            Console.WriteLine("0<X<(+)Бесконечность, -Бесконечность<Y<0");
+            break;
+    }
+else
+{
+    Console.WriteLine("wrong try adain");
+}
